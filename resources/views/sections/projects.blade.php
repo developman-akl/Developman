af0a9914192c66a97f4b5f4e924317edb76f12a0
molnarjg <molnarjg@gmail.com>
2021-07-21 19:37:37 +1200

    Project left/right arrows

--- resources/views/sections/projects.blade.php
+++ resources/views/sections/projects.blade.php
@@ -33,10 +33,10 @@
                 })"
         >
             <div class="absolute inset-y-0 left-0 z-10 flex items-center">
-                <button @click="swiper.slidePrev()" class="lg:-ml-4 text-green focus:outline-none flex items-center justify-center w-10 h-10 -ml-2 bg-white rounded-full shadow">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                    </svg>
+                <button @click="swiper.slidePrev()" class="hover:text-[#0050D1] transform hover:scale-125 duration-700 lg:-ml-4 text-green focus:outline-none flex items-center justify-center w-10 h-10 -ml-2 bg-white rounded-full shadow text-[#001E4F]">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
+                      </svg>
                 </button>
             </div>
     
@@ -55,7 +55,7 @@
                                         {!! 'Javasolj!<br><br>' !!}
                                     </h2>
                                     <article class=" max-w-full px-2 mt-2 mr-1 prose text-gray-700 break-words">
-                                        {!! 'Web Application for everyday inquiries and commission for the winner proposals.<br><br><br>' !!}
+                                        {!! 'Web Application for general inquiries with commission for the winner proposals.<br><br><br>' !!}
                                     </article> 
                                 </div>
                                 <div class="justify-evenly sm:flex-row sm:space-y-0 flex flex-col items-center py-1 space-y-2 border-t">
@@ -80,41 +80,9 @@
                         </div>
                     </div>  
 
-                    {{-- SeiyraDesign --}}
-                    <div class="swiper-slide p-1 transition duration-1000">
-                        <div class="max-w-2xl p-2 mx-auto">
-                            <div class="shadow-news rounded-xl bg-gray-50 mb-6 tracking-wider border" >
-                                <div class="md:flex-shrink-0 border-b">
-                                    <img src="{{ asset('storage/images/project_seiyradesign_0.png') }}" alt="kep1" class="h-60 rounded-xl object-cover w-full rounded-b-none">
-                                </div>
-                                <div class="cursor-help relative px-4 py-2 mt-2 text-center">
-                                    <h2 class="font-content text-lg font-extrabold tracking-normal text-gray-800">
-                                        {!! 'SeiyraDesign<br><br>' !!}
-                                    </h2>
-                                    <article class=" max-w-full px-2 mt-2 mr-1 prose text-gray-700 break-words">
-                                        {!! 'The official web page of SeiyraDesign.<br><br><br><br>' !!}
-                                    </article> 
-                                </div>
-                                <div class="justify-evenly sm:flex-row sm:space-y-0 flex flex-col items-center py-1 space-y-2 border-t">
-                                    <div class="flex items-center justify-center mx-auto">
-                                        <img class="h-4" src="https://www.w3.org/html/logo/badge/html5-badge-h-solo.png" alt="HTML5 Powered" title="HTML5 Powered">
-                                    </div>
-                                    <div class="flex items-center justify-center mx-auto">
-                                        <img class="h-4" src="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg" alt="HTML5 Powered" title="SASS">
-                                    </div>
-                                    <div class="flex items-center justify-center mx-auto">
-                                        <img class="h-4" src="https://upload.wikimedia.org/wikipedia/commons/f/fd/JQuery-Logo.svg" alt="JQuery Logo" title="JQuery" />
-                                    </div>
-                                    <div class="flex items-center justify-center mx-auto space-x-1">
-                                        <img class="h-4" src="https://laravel.com/img/logomark.min.svg" alt="Laravel Logo" title="Laravel" />
-                                        <img class="h-3" src="https://laravel.com/img/logotype.min.svg" alt="Laravel Logo" title="Laravel" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>  
+                    
 
-                    {{-- Novaerus --}}
+                    {{-- BMS --}}
                     <div class="swiper-slide p-1 transition duration-1000">
                         <div class="max-w-2xl p-2 mx-auto">
                             <div class="shadow-news rounded-xl bg-gray-50 mb-6 tracking-wider border" >
@@ -123,10 +91,10 @@
                                 </div>
                                 <div class="cursor-help relative px-4 py-2 mt-2 text-center">
                                     <h2 class="font-content text-lg font-extrabold tracking-normal text-gray-800">
-                                        {!! 'Novaerus BMS<br><br>' !!}
+                                        {!! 'Inventory & Stock Management<br><br>' !!}
                                     </h2>
                                     <article class=" max-w-full px-2 mt-2 mr-1 prose text-gray-700 break-words">
-                                        {!! 'Business Management Software of Novaerus which is an airfilter vendor and service company.<br><br>' !!}
+                                        {!! 'Business Management Software for an airfiltration machines vendor and servicing company.<br><br>' !!}
                                     </article> 
                                 </div>
                                 <div class="justify-evenly sm:flex-row sm:space-y-0 flex flex-col items-center py-1 space-y-2 border-t">
@@ -149,7 +117,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>  
+                    </div> 
 
                     {{-- Manufacturing --}}
                     <div class="swiper-slide p-1 transition duration-1000">
@@ -160,10 +128,10 @@
                                 </div>
                                 <div class="cursor-help relative px-4 py-2 mt-2 text-center">
                                     <h2 class="font-content text-lg font-extrabold tracking-normal text-gray-800">
-                                        {!! 'Medical Shoes Manufacturing BMS<br><br>' !!}
+                                        {!! 'Medical Shoes Manufacturing<br><br>' !!}
                                     </h2>
                                     <article class=" max-w-full px-2 mt-2 mr-1 prose text-gray-700 break-words">
-                                        {!! 'Business Management Software for a bakery to make the everyday task management a breeze.<br><br>' !!}
+                                        {!! 'Business Management Software for a medical shoes manufacturing company.<br><br><br>' !!}
                                     </article> 
                                 </div>
                                 <div class="justify-evenly sm:flex-row sm:space-y-0 flex flex-col items-center py-1 space-y-2 border-t">
@@ -184,6 +152,77 @@
                             </div>
                         </div>
                     </div> 
+
+                    {{-- SeiyraDesign --}}
+                    <div class="swiper-slide p-1 transition duration-1000">
+                        <div class="max-w-2xl p-2 mx-auto">
+                            <div class="shadow-news rounded-xl bg-gray-50 mb-6 tracking-wider border" >
+                                <div class="md:flex-shrink-0 border-b">
+                                    <img src="{{ asset('storage/images/project_seiyradesign_0.png') }}" alt="kep1" class="h-60 rounded-xl object-cover w-full rounded-b-none">
+                                </div>
+                                <div class="cursor-help relative px-4 py-2 mt-2 text-center">
+                                    <h2 class="font-content text-lg font-extrabold tracking-normal text-gray-800">
+                                        {!! 'SeiyraDesign<br><br>' !!}
+                                    </h2>
+                                    <article class=" max-w-full px-2 mt-2 mr-1 prose text-gray-700 break-words">
+                                        {!! 'The official website of SeiyraDesign.<br><br><br><br>' !!}
+                                    </article> 
+                                </div>
+                                <div class="justify-evenly sm:flex-row sm:space-y-0 flex flex-col items-center py-1 space-y-2 border-t">
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <img class="h-4" src="https://www.w3.org/html/logo/badge/html5-badge-h-solo.png" alt="HTML5 Powered" title="HTML5 Powered">
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <img class="h-4" src="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg" alt="SASS" title="SASS">
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <img class="h-4" src="https://upload.wikimedia.org/wikipedia/commons/f/fd/JQuery-Logo.svg" alt="JQuery Logo" title="JQuery" />
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto space-x-1">
+                                        <img class="h-4" src="https://laravel.com/img/logomark.min.svg" alt="Laravel Logo" title="Laravel" />
+                                        <img class="h-3" src="https://laravel.com/img/logotype.min.svg" alt="Laravel Logo" title="Laravel" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+
+                    {{-- Developman --}}
+                    <div class="swiper-slide p-1 transition duration-1000">
+                        <div class="max-w-2xl p-2 mx-auto">
+                            <div class="shadow-news rounded-xl bg-gray-50 mb-6 tracking-wider border" >
+                                <div class="md:flex-shrink-0 border-b">
+                                    <img src="{{ asset('storage/images/project_developman.png') }}" alt="kep1" class="h-60 rounded-xl object-cover w-full rounded-b-none">
+                                </div>
+                                <div class="cursor-help relative px-4 py-2 mt-2 text-center">
+                                    <h2 class="font-content text-lg font-extrabold tracking-normal text-gray-800">
+                                        {!! 'Developman website<br><br>' !!}
+                                    </h2>
+                                    <article class=" max-w-full px-2 mt-2 mr-1 prose text-gray-700 break-words">
+                                        {!! 'The official website of Developman.<br><br><br><br>' !!}
+                                    </article> 
+                                </div>
+                                <div class="justify-evenly sm:flex-row sm:space-y-0 flex flex-col items-center py-1 space-y-2 border-t">
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <img class="h-4" src="https://www.w3.org/html/logo/badge/html5-badge-h-solo.png" alt="HTML5 Powered" title="HTML5 Powered">
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <img class="h-4" src="https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg" alt="SASS" title="SASS">
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <x-svg.icons.tailwind />
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto">
+                                        <img class="h-4" src="https://alpinejs.dev/alpine_long.svg" alt="AlpineJS Logo" title="AlpineJS" />
+                                    </div>
+                                    <div class="flex items-center justify-center mx-auto space-x-1">
+                                        <img class="h-4" src="https://laravel.com/img/logomark.min.svg" alt="Laravel Logo" title="Laravel" />
+                                        <img class="h-3" src="https://laravel.com/img/logotype.min.svg" alt="Laravel Logo" title="Laravel" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
 
                     {{-- Task Manager --}}
                     <div class="swiper-slide p-1 transition duration-1000">
@@ -226,9 +265,9 @@
             </div>
     
             <div class="absolute inset-y-0 right-0 z-10 flex items-center">
-                <button @click="swiper.slideNext()" class="lg:-mr-4 focus:outline-none text-green flex items-center justify-center w-10 h-10 -mr-2 bg-white rounded-full shadow">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                <button @click="swiper.slideNext()" class="hover:text-[#0050D1] transform hover:scale-125 duration-700 lg:-mr-4 focus:outline-none text-green flex items-center justify-center w-10 h-10 -mr-2 bg-white rounded-full shadow text-[#001E4F]">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
                     </svg>
                 </button>
             </div>
